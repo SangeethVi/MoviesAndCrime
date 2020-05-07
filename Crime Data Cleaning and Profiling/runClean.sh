3e1a5179bb9f3130f84e4dda327b19958734c2c9
concon55 <clg469@nyu.edu>
2020-05-07 14:19:07 -0700

    Delete runClean.sh

--- Crime Data Cleaning and Profiling/runClean.sh
+++
@@ -1,8 +0,0 @@
-javac -classpath `yarn classpath` -d . CleanMapper.java
-javac -classpath `yarn classpath` -d . CleanReducer.java
-javac -classpath `yarn classpath`:. -d . Clean.java
-
-jar -cvf clean.jar *.class
-
-hadoop jar clean.jar Clean /user/clg469/project/table-1.csv /user/clg469/project/output
-
